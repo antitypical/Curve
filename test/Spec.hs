26d57c12b0946bc16c08da92cdec68310e112c48
Rob Rix <rob.rix@github.com>
2016-01-09 17:20:15 -0500

    Stub in an Arbitrary instance for Term'.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,9 @@
+{-# LANGUAGE FlexibleInstances #-}
+import Test.QuickCheck
+import Curve
+
+instance Arbitrary (Term Expression) where
+  arbitrary = elements [ Term Type ]
+
 main :: IO ()
 main = putStrLn "Test suite not yet implemented"
