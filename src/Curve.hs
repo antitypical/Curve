761c27881f6c8e104088380d0030a1d24e27ac19
Rob Rix <rob.rix@github.com>
2016-01-09 12:50:15 -0500

    Variables unify.

--- src/Curve.hs
+++ src/Curve.hs
@@ -28,6 +28,8 @@ unify expected actual = case (out expected, out actual) of
   (_, Implicit) -> into expected
 
   (Type, Type) -> into expected
+
+  (Variable n1, Variable n2) | n1 == n2 -> Unification $ Variable n2
   (Application a1 b1, Application a2 b2) -> Unification $ Application (unify a1 a2) (unify b1 b2)
   (Lambda i1 a1 b1, Lambda i2 a2 b2) | i1 == i2 -> Unification $ Lambda i2 (unify a1 a2) (unify b1 b2)
 
