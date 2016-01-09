3feae5ed9d0a1f6dfd2e3e633df11ce1982641ea
Rob Rix <rob.rix@github.com>
2016-01-09 18:13:07 -0500

    Revert "Rename the identical terms test."
    This reverts commit 2e3f74da2d0ba11bd1106ef5b3342d48ed2d087c.

--- test/Spec.hs
+++ test/Spec.hs
@@ -25,5 +25,5 @@ main = hspec $ do
     prop "x with _" $
       \ term -> Term Implicit `unify` term `shouldBe` into term
 
-    prop "identical terms" $
+    prop "reflexivity" $
       \ term -> unify term term `shouldBe` into term
