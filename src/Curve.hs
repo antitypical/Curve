6f9b62082736cf1ee8cb5d9693bd9a478826eac5
Rob Rix <rob.rix@github.com>
2016-01-09 12:42:58 -0500

    Add a getter for the contents of a Term.

--- src/Curve.hs
+++ src/Curve.hs
@@ -8,7 +8,7 @@ data Expression term
   | Application term term
   deriving (Show, Eq, Functor, Foldable)
 
-data Term f = Term (f (Term f))
+data Term f = Term { out :: f (Term f) }
 type Term' = Term Expression
 
 data Unification f = Unification (f (Unification f)) | Conflict (Term f) (Term f)
