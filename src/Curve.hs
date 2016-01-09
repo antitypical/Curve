e9a20107b8bd6bee23c5b25ca49bd3cef733a8bc
Rob Rix <rob.rix@github.com>
2016-01-09 17:07:47 -0500

    Add an instance of Eq for Term f where EqBy f.

--- src/Curve.hs
+++ src/Curve.hs
@@ -29,6 +29,9 @@ instance EqBy Expression where
 data Term f = Term { out :: f (Term f) }
 type Term' = Term Expression
 
+instance EqBy f => Eq (Term f) where
+  a == b = eqBy (==) (out a) (out b)
+
 data Unification f = Unification (f (Unification f)) | Conflict (Term f) (Term f)
 type Unification' = Unification Expression
 
