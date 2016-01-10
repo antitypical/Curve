c0e8134b31132a1d4f052a517308161ad472f68e
Rob Rix <rob.rix@github.com>
2016-01-10 12:50:48 -0500

    Move unification into its own section.

--- src/Curve.hs
+++ src/Curve.hs
@@ -96,19 +96,6 @@ substitute name withTerm inScope = case out inScope of
   Application inA inB -> Term $ Application (substitute name withTerm inA) (substitute name withTerm inB)
   _ -> inScope
 
-unify :: Term' -> Term' -> Unification'
-unify expected actual = case (out expected, out actual) of
-  (_, Implicit) -> into expected
-  (Implicit, _) -> into actual
-
-  (Type, Type) -> into expected
-
-  (Variable n1, Variable n2) | n1 == n2 -> Unification $ Variable n2
-  (Application a1 b1, Application a2 b2) -> Unification $ Application (unify a1 a2) (unify b1 b2)
-  (Lambda i1 a1 b1, Lambda i2 a2 b2) | i1 == i2 -> Unification $ Lambda i2 (unify a1 a2) (unify b1 b2)
-
-  _ -> Conflict expected actual
-
 
 freeVariables :: Term' -> Set.Set Name
 freeVariables = cata inExpression
@@ -123,6 +110,22 @@ maxBoundVariable = cata (\ expression -> case expression of
   Lambda n t _ -> max (Just n) t
   Application a b -> max a b
   _ -> Nothing)
+
+
+-- Equality
+
+unify :: Term' -> Term' -> Unification'
+unify expected actual = case (out expected, out actual) of
+  (_, Implicit) -> into expected
+  (Implicit, _) -> into actual
+
+  (Type, Type) -> into expected
+
+  (Variable n1, Variable n2) | n1 == n2 -> Unification $ Variable n2
+  (Application a1 b1, Application a2 b2) -> Unification $ Application (unify a1 a2) (unify b1 b2)
+  (Lambda i1 a1 b1, Lambda i2 a2 b2) | i1 == i2 -> Unification $ Lambda i2 (unify a1 a2) (unify b1 b2)
+
+  _ -> Conflict expected actual
 
 
 -- Recursion schemes
